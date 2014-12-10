cb18add5a2c1861fcea5ce857685da60e1ba52ec
大兵 <@LI-PC>
2014-12-10 16:28:40 +0800

    修改图标，左侧菜单项

--- src/com/iumol/kanmeizi/dao/ImageClass.java
+++ src/com/iumol/kanmeizi/dao/ImageClass.java
@@ -14,33 +14,41 @@ public class ImageClass {
 			{ "美女自拍", "http://www.mzitu.com/tag/%E8%87%AA%E6%8B%8D/page/" },
 			{ "网络美女", "http://www.mnchao.com/wlmn_%1$s.html" },
 			{ "清纯唯美", "http://www.mnchao.com/qcwm_%1$s.html" },
-
 			{ "清纯校花", "http://www.mzitu.com/tag/%E6%A0%A1%E8%8A%B1/page/" },
-			{ "小清新", "http://www.mzitu.com/mm/page/" },
-			{ "萌妹子", "http://www.mnchao.com/mmz_%1$s.html" } };
-	// {
-	// "魅力街拍","http://www.weimei58.com/forum.php?mod=forumdisplay&fid=161&page="
-	// },
+			{ "丝袜美腿", "http://www.mnchao.com/swmt_%1$s.html" },
+			{ "ROSI咪咪", "http://www.mzitu.com/tag/rosi/page/" },
+
+	/*
+	 * { "小清新", "http://www.mzitu.com/mm/page/" }, { "萌妹子",
+	 * "http://www.mnchao.com/mmz_%1$s.html" }
+	 */};
+
 	public static int[] imageDrawable = { R.drawable.gengduo,
 			R.drawable.taonvlang, R.drawable.douban, R.drawable.weibo,
 			R.drawable.bijini, R.drawable.jiepai, R.drawable.zipai,
 			R.drawable.wangluo, R.drawable.yougou, R.drawable.xiaohua,
-			R.drawable.xiaoqingxin, R.drawable.mengmeizi };
+			R.drawable.meitun, R.drawable.rosi /*
+												 * , R.drawable.xiaoqingxin,
+												 * R.drawable.mengmeizi
+												 */};
 
-	public static String[] MenuImageTitle = { "台湾美女", "日本系列", "湿身诱惑", "美臀系列",
-			"丝袜美腿", "魅力模特", "性感美女", "ROSI咪咪", "有沟必火", "关于我们" };
+	public static String[] MenuImageTitle = { /*
+											 * "台湾美女", "日本系列", "湿身诱惑", "美臀系列",
+											 * "丝袜美腿", "魅力模特", "性感美女", "ROSI咪咪",
+											 * "有沟必火"
+											 */"热门游戏", "两性话题", "情趣商城", "关于我们" };
 	public static String[] MenuImageUrl = {
 
-			"http://www.mzitu.com/taiwan/page/",
-			"http://www.mzitu.com/japan/page/",
-			"http://www.mzitu.com/model/page/",
-			"http://www.mzitu.com/tag/%E7%BE%8E%E8%87%80/page/",
-			"http://www.mnchao.com/swmt_%1$s.html",
-			"http://www.mnchao.com/mlmt_%1$s.html",
-			"http://www.mnchao.com/xgmn_%1$s.html",
-			"http://www.mzitu.com/tag/%E6%9C%89%E6%B2%9F%E5%BF%85%E7%81%AB/page/",
-			"http://www.mzitu.com/tag/rosi/page/",
-
+	"http://kkxyx.com/", "http://m.sohu.com/cl/85/?v=3",
+			"http://wap.chunshuitang.com"
+	/*
+	 * "http://www.mzitu.com/taiwan/page/", "http://www.mzitu.com/japan/page/",
+	 * "http://www.mzitu.com/model/page/",
+	 * "http://www.mzitu.com/tag/%E7%BE%8E%E8%87%80/page/",
+	 * "http://www.mnchao.com/mlmt_%1$s.html",
+	 * "http://www.mnchao.com/xgmn_%1$s.html",
+	 * "http://www.mzitu.com/tag/%E6%9C%89%E6%B2%9F%E5%BF%85%E7%81%AB/page/",
+	 */
 	};
 }
 
